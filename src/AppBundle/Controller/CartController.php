d6299ceebd13c58e9deec21a7c1bff7cd785e32e
Sami <sami.ch.27@gmail.com>
2018-04-29 11:49:47 +0100

    Add to cart and go back to products list

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -5,8 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\HttpFoundation\Session\Session;
+
+use AppBundle\Entity\Product;
 /**
  * @Route("/eshop")
  */
@@ -20,6 +23,16 @@ class CartController extends Controller
      	@$cart = $session->get('sess_cart');
      	@$cart[$product_id] ++ ;
      	$session->set('sess_cart', $cart);
+     	
+     	// Retrouner à la page précédente
+     	$em   = $this->getDoctrine()->getManager();
+     	$repo = $em->getRepository(Product::class);
+     	$prd  = $repo->find($product_id);
+     	$cat  = $prd->getCategory();
+     	return $this->redirectToRoute('eshop_category', ['id'=> $cat->getId()]);
+     }
+
+     function cartCount() {
      	
      }
 }

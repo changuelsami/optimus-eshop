521307f092292d95b180735d9b8aaaaefe16dac2
Sami <sami.ch.27@gmail.com>
2018-05-01 12:46:37 +0100

    save order data

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Category;
 use AppBundle\Entity\Product;
 use AppBundle\Entity\Commande;
+use AppBundle\Entity\ProductCommande;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -21,7 +22,7 @@ class OrderController extends Controller
     /**
      * @Route("/order/", name="eshop_order")
      */
-    public function order()
+    public function order(Request $request)
     {
     	// Build oder form    
         $commande = new Commande();  
@@ -41,7 +42,39 @@ class OrderController extends Controller
                         ])
                     ->getForm();
         
+        $form->handleRequest($request);
+        if($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            // Sauvegarder la commande (données client)
+            $em->persist($commande);
+
+            // Boucle pour Sauvegarder TOUS les produits du panier
+            $session = new Session();
+            $cart = $session->get('sess_cart');
+            $repo = $em->getRepository(Product::class);
+            foreach ($cart as $prd_id => $qty) {
+                $pc = new ProductCommande();
+                $pc->setQty($qty);
+                $prd = $repo->find($prd_id);
+                $pc->setPrice($prd->getPrice());
+                $pc->setProduct($prd);
+                $pc->setCommande($commande);
+
+                $em->persist($pc);
+            }
+            $em->flush();
+            $num_cmd = $commande->getId();
+            return $this->redirectToRoute('eshop_merci', ['num_cmd' => $num_cmd]);
+        }
+
         return $this->render("eshop/front/order.html.twig", ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route('/merci/{order_id', name="eshop_merci")
+     */
+    public function merci($order_id) {
+        
     }
 
 }
